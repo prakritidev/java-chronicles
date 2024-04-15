f3e245e6c9a247359522b34037e3f7b2725b673a
Prakrtidev Verma <prakritidevverma@gmail.com>
2024-04-15 23:14:05 +0530

    feat: added binary search on answers

---
+++ binarysearch/onanswers/FindSquareRoot.java
@@ -0,0 +1,23 @@
+package binarysearch.onanswers;
+
+public class FindSquareRoot {
+    public static void main(String[] args) {
+        System.out.println(getSquareRoot(125, 2));
+    }
+
+    private static float getSquareRoot(int i, int j) {
+        float low = 1, high = i;
+
+        while (low <= high) {
+            float mid = low + (high - low) / 2;
+            if(Math.pow(mid, j) == i) return mid;
+            if(Math.pow(mid, j) >= (long)(i)) {
+                high = (int)(mid -1);
+            } else {
+                low = (int)(mid + 1);
+            }
+        }
+
+        return -1;
+    }
+}
