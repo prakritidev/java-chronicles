67c59f177d935f6eccf852772dc0a37546c8c04d
Prakrtidev Verma <prakritidevverma@gmail.com>
2024-07-05 00:42:21 +0530

    Local Sync: 2024-07-05 00:42:21

--- greedy/FractionalKnapsack.java
+++ greedy/FractionalKnapsack.java
@@ -1,15 +1,19 @@
 package greedy;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Item
  */
 class Item {
-  int value, weight;
-  Item(int value, int weight) {
-    this.value = value;
+  double weight;
+  double value;
+
+  // Constructor
+  public Item(double weight, double value) {
     this.weight = weight;
+    this.value = value;
   }
 }
 
@@ -21,19 +25,44 @@ public class FractionalKnapsack {
 
   public static void main(String[] args) {
     int n = 5;
-    int weight = 50;
-    Item arr[] = {new Item (100,20),new Item(60,10),new Item(120,30)};
-    double ans = fractionalKnapsack(weight, arr, n);
-    System.out.println("The maximum value is "+ans);
-
+    int capacity = 50;
+    Item[] items = { 
+      new Item(10, 60), 
+      new Item(20, 100), 
+      new Item(30, 120) 
+  };
+  
+  double maxValue = fractionalKnapsack(capacity, items);
+  System.out.println("Maximum value we can obtain = " + maxValue);
 
   }
 
-  private static double fractionalKnapsack(int weight, Item[] arr, int n) {
-    Arrays.sort(arr);
-    
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'fractionalKnapsack'");
+  private static double fractionalKnapsack(int capacity, Item[] items) {
+
+    Arrays.sort(items, new Comparator<Item>() {
+      @Override
+      public int compare(Item o1, Item o2) {
+        double r1 = o1.value / o2.weight;
+        double r2 = o2.value / o2.weight;
+        return Double.compare(r1, r2);
+      }
+
+    });
+    double totalValue = 0.0;
+    for (Item item : items) {
+      int curWt = (int) item.weight;
+      int curVal = (int) item.value;
+      if (capacity - curWt >= 0) {
+        capacity -= curWt;
+        totalValue += curVal;
+      } else {
+        double fraction = ((double) capacity / (double) curWt);
+        totalValue += (curVal * fraction);
+        capacity = (int) (capacity - (curWt * fraction));
+        break;
+      }
+    }
+    return totalValue;
   }
 
 }
