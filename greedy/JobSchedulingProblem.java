e558f801211c4e73c72df58f9fcd879e5bcdd8da
Prakrtidev Verma <prakritidevverma@gmail.com>
2024-07-12 02:12:50 +0530

    Local Sync: 2024-07-12 02:12:50

---
+++ greedy/JobSchedulingProblem.java
@@ -0,0 +1,34 @@
+package greedy;
+
+/**
+ * InnerJobSchedulingProblem
+ */
+class InnerJobSchedulingProblem {
+    int id, profit, deadline;
+
+    public InnerJobSchedulingProblem() {
+    }
+
+    public InnerJobSchedulingProblem(int id, int profit, int deadline) {
+        this.id = id;
+        this.profit = profit;
+        this.deadline = deadline;
+    }
+
+}
+
+public class JobSchedulingProblem {
+    public static void main(String[] args) {
+        System.out.println("JobSchedulingProblem.main()");
+        InnerJobSchedulingProblem[] arr = new InnerJobSchedulingProblem[4];
+        arr[0] = new InnerJobSchedulingProblem(1, 4, 20);
+        arr[1] = new InnerJobSchedulingProblem(2, 1, 10);
+        arr[2] = new InnerJobSchedulingProblem(3, 2, 40);
+        arr[3] = new InnerJobSchedulingProblem(4, 2, 30);
+
+    }
+
+    public static String scheduleJobs(InnerJobSchedulingProblem[] input) {
+        return null;
+    }
+}
--- greedy/JumpGame.java
+++ greedy/JumpGame.java
@@ -1,17 +1,30 @@
+package greedy;
 
 public class JumpGame {
 
-    public static void main(String[] args) {
-        int[] nums= {2,3,1,1,4};
-        System.err.println("Output: " + canJump(nums));
-    }
+  public static void main(String[] args) {
+    int[] nums = { 2, 3, 1, 1, 4 };
+    System.err.println("Output: " + canJump(nums));
+    System.err.println("Output: " + canJumpMin(nums));
+  }
 
-    private static boolean canJump(int[] nums) {
-        int current = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(i > current) return false;
-            current = Math.max(current, i + nums[i]);
-        }
-        return true;
+  private static boolean canJump(int[] nums) {
+    int current = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i > current)
+        return false;
+      current = Math.max(current, i + nums[i]);
     }
+    return true;
+  }
+
+  private static int canJumpMin(int[] nums) {
+    int current = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i > current)
+        return 0;
+      current = Math.max(current, i + nums[i]);
+    }
+    return 0;
+  }
 }
