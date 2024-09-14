810870615c97afd1bfe08934bb5ddec8902af47b
Prakrtidev Verma <prakritidevverma@gmail.com>
2024-09-14 16:01:36 +0530

    Local Sync: 2024-09-14 16:01:36

--- graphquestions/DFS.java
+++ graphquestions/DFS.java
@@ -31,13 +31,13 @@ public class DFS {
     public static void main(String[] args) {
         DFS graph = new DFS();
         // Add edges to the graph
-        graph.addEdge(1, 2);
         graph.addEdge(1, 3);
+        graph.addEdge(1, 2);
         graph.addEdge(2, 4);
         graph.addEdge(3, 5);
         graph.addEdge(4, 6);
         graph.addEdge(5, 6);
-
+        
         graph.dfs(1);
     }
 }
---
+++ leetcode75/MergeStringAlternative.java
@@ -0,0 +1,39 @@
+
+public class MergeStringAlternative {
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "defsdfghjk";
+
+        String result = getAlternativeString(s1, s2);
+        System.out.println(result);
+    }
+
+    public static String getAlternativeString(String s1, String s2) {
+
+        Integer l1 = s1.length(), l2 = s2.length();
+        int i = 0, j = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i < l1 && j < l2) {
+
+            sb.append(s1.charAt(i));
+            i++;
+            sb.append(s2.charAt(j));
+            j++;
+        }
+
+        while (i < l1) {
+            sb.append(s1.charAt(i));
+            i++;
+        }
+
+        while (j < l2) {
+            sb.append(s2.charAt(j));
+            j++;
+        }
+        return sb.toString();
+
+    }
+}
